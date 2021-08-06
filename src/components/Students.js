032f051812312766036925cd2c1c18121f30ac3c
Steven Saunders <steve0183@gmail.com>
2021-08-06 13:46:58 -0600

    Installed SASS and added some classes for styling

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,27 +1,29 @@
-
 const Students = ({ search, filteredStudents, handleChange }) => {
-
 	return (
-		<main>
+		<div className='student-search-wrapper'>
 			<input
 				type='text'
 				value={search}
 				placeholder='Search Students'
 				onChange={handleChange}
 			/>
-
-			<div>
-				<ul>
-					{filteredStudents.map((student) => {
-						return (
-							<li key={student.id}>
-								<img src={student.pic} alt={`${student.firstName} profile pic`} /> {student.firstName} {student.lastName}
-							</li>
-						)
-					})}
-				</ul>
-			</div>
-		</main>
+			<ul className='student-list'>
+				{filteredStudents.map((student) => {
+					return (
+						<li className='student-item' key={student.id}>
+							<img
+								className='student-avatar'
+								src={student.pic}
+								alt={`${student.firstName} profile pic`}
+							/>
+							<div className='student-content'>
+								{student.firstName} {student.lastName}
+							</div>
+						</li>
+					)
+				})}
+			</ul>
+		</div>
 	)
 }
 

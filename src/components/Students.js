fa9ced93a1a1bf5d75af6c60b47eae120192fb2e
Steven Saunders <steve0183@gmail.com>
2021-08-09 01:02:31 -0600

    Add show/hide button for grades list. Add state for isHidden for conditional render

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,27 +1,87 @@
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
 const Students = ({ search, filteredStudents, handleChange }) => {
+	const [isHidden, setIsHidden] = useState(true)
+
+	const toggle = () => {
+		setIsHidden(!isHidden)
+	}
 
 	return (
-		<main>
+		<div className='student-search-wrapper'>
 			<input
 				type='text'
 				value={search}
 				placeholder='Search Students'
 				onChange={handleChange}
 			/>
+			<ul className='student-list'>
+				{filteredStudents.map((student) => {
+					const averageGrade =
+						student.grades.reduce((accumulator, currentValue) => {
+							return +currentValue + +accumulator
+						}) / student.grades.length
 
-			<div>
-				<ul>
-					{filteredStudents.map((student) => {
-						return (
-							<li key={student.id}>
-								<img src={student.pic} alt={`${student.firstName} profile pic`} /> {student.firstName} {student.lastName}
-							</li>
-						)
-					})}
-				</ul>
-			</div>
-		</main>
+					return (
+						<li className='student-item' key={student.id}>
+							<div className='image-wrapper'>
+								<img
+									className='student-avatar'
+									src={student.pic}
+									alt={`${student.firstName} profile pic`}
+								/>
+							</div>
+							<div className='info-wrapper'>
+								<h3 className='student-content'>
+									{student.firstName} {student.lastName}
+								</h3>
+								<ul>
+									<li>{student.email}</li>
+									<li>{student.company}</li>
+									<li>{student.skill}</li>
+									<li>{averageGrade.toFixed(3)}</li>
+								</ul>
+								<div
+									className='grades-wrapper'
+									hidden={isHidden}
+								>
+									<ul className='grades-list'>
+										{student.grades.map((grade, index) => {
+											return (
+												<li
+													className='grade-item'
+													key={index}
+												>
+													Test {(index += 1)}: {grade}
+													%
+												</li>
+											)
+										})}
+									</ul>
+								</div>
+								<div className='toggle-wrapper'>
+									<button onClick={toggle}>
+										{isHidden === true ? (
+											<FontAwesomeIcon
+												icon={faPlus}
+												size='3x'
+											/>
+										) : (
+											<FontAwesomeIcon
+												icon={faMinus}
+												size='3x'
+											/>
+										)}
+									</button>
+								</div>
+							</div>
+						</li>
+					)
+				})}
+			</ul>
+		</div>
 	)
 }
 
